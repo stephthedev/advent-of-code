6cf4ff0555b3cb72a6e745e2bf3b032d4051c1cd
Stephanie Ortiz <stephthedev@gmail.com>
2022-01-25 14:59:06 -0500

    Initial 2021 work

---
+++ src/main/java/com/stephthedev/aoc/Day3.java
@@ -0,0 +1,119 @@
+package com.stephthedev.aoc;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day3 {
+
+    private enum BitCriteriaRating {
+        OXYGEN, C02
+    }
+
+    //Given a list of binary numbers, process the gamma rate and epsilon rate.
+    private int processPowerConsumption(List<String> binaryNumbers) {
+        int length = binaryNumbers.get(0).length();
+
+        //Note: To process either the gamma rate or epsilon rate, it's only necessary to
+        // count the frequency of 0s OR 1s (because it's a binary number)
+        // So, we only only keep track of the number of 1s and infer the number of 0s
+        // How? To get the number of 0s, we do: total - (1 frequency) to get a 0 frequency
+
+        //Keep a map of index position and number of ones it accumulates
+        Map<Integer, Integer> gammaRateOnlyOnesMap = new HashMap<>(length);
+        for (int i=0; i<length; i++) {
+            gammaRateOnlyOnesMap.put(i, 0);
+        }
+
+        //For each index, increase the count for the number of times a 1 was found
+        for (String number : binaryNumbers) {
+            for (int i=0; i<number.length(); i++) {
+                char digit = number.charAt(i);
+                if (digit == '1') {
+                    int prevTotal = gammaRateOnlyOnesMap.get(i) + 1;
+                    gammaRateOnlyOnesMap.put(i, prevTotal);
+                }
+            }
+        }
+
+        int half = binaryNumbers.size() / 2;
+        StringBuilder gammaRateBuilder = new StringBuilder();
+        StringBuilder epsilonRateBuilder = new StringBuilder();
+        for (int i=0; i<length; i++) {
+            int onesTotal = gammaRateOnlyOnesMap.get(i);
+            int highestDigit = -1;
+            int lowestDigit = -1;
+
+            //If the total number of ones is larger than half the list of numbers (500), it's rate is higher
+            if (onesTotal > half) {
+                highestDigit = 1;
+                lowestDigit = 0;	//We can then infer that 0 is lower
+            } else {
+                highestDigit = 0;
+                lowestDigit = 1;	//We can then infer this
+            }
+            gammaRateBuilder = gammaRateBuilder.append(highestDigit);
+            epsilonRateBuilder = epsilonRateBuilder.append(lowestDigit);
+        }
+        int gammaRate = binaryStringToDecimal(gammaRateBuilder);
+        int epsilonRate = binaryStringToDecimal(epsilonRateBuilder);
+        return gammaRate * epsilonRate;
+    }
+
+    private int processLifeSupportRating(List<String> binaryNumbers) {
+        int oxygenGeneratorRating = processBitCriteria(binaryNumbers, BitCriteriaRating.OXYGEN);
+        int co2ScrubberRating = processBitCriteria(binaryNumbers, BitCriteriaRating.C02);
+        return oxygenGeneratorRating * co2ScrubberRating;
+    }
+
+    private int processBitCriteria(List<String> binaryNumbersAsStrings, BitCriteriaRating rating) {
+        int strLength = binaryNumbersAsStrings.get(0).length();
+        List<String> numbers = new ArrayList<>();
+        numbers.addAll(binaryNumbersAsStrings);
+        for (int i=0; i<strLength; i++) {
+            numbers = filterArray(numbers, i, rating);
+            if (numbers.size() == 1) {
+                break;
+            }
+        }
+        return binaryStringToDecimal(numbers.get(0));
+    }
+
+    //Filters an array according to the bit criteria
+    private List<String> filterArray(List<String> binaryNumbers, int index, BitCriteriaRating rating) {
+        long zeroesCount = binaryNumbers.stream().filter(s -> s.charAt(index) == '0').count();
+        long onesCount = binaryNumbers.stream().filter(s -> s.charAt(index) == '1').count();
+        char c = '1';
+        if (rating == BitCriteriaRating.OXYGEN) {
+            c = zeroesCount > onesCount ? '0' : '1';
+        } else if (rating == BitCriteriaRating.C02) {
+            c = zeroesCount <= onesCount ? '0' : '1';
+        }
+        char oneOrZero = c;
+        return binaryNumbers.stream().filter( s -> s.charAt(index) == oneOrZero).collect(Collectors.toList());
+    }
+
+    private int binaryStringToDecimal(StringBuilder builder) {
+        return binaryStringToDecimal(builder.toString());
+    }
+
+    private int binaryStringToDecimal(String binaryStr) {
+        int number = Integer.parseInt(binaryStr, 2);
+        //System.out.println(String.format("Binary: %s, Number: %d", binaryStr, number));
+        return number;
+    }
+
+    public long[] run(String fileName) throws FileNotFoundException, IOException {
+        List<String> binaryNumbers = Util.parseFileAsStringArray(fileName);
+        int part1 = processPowerConsumption(binaryNumbers);
+        int part2 = processLifeSupportRating(binaryNumbers);
+        return new long[] {part1, part2};
+    }
+
+    public static void main(String[] args) throws IOException {
+        Day3 day = new Day3();
+        long[] solution = day.run("src/main/resources/day3_input.txt");
+        System.out.println(Arrays.toString(solution));
+    }
+}
---
+++ src/main/java/com/stephthedev/aoc/Day5.java
@@ -0,0 +1,195 @@
+package com.stephthedev.aoc;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Day5 {
+
+    public List<Line> parseData(String fileName) throws FileNotFoundException, IOException {
+        List<String> linesData = Util.parseFileAsStringArray(fileName);
+        List<Line> lines = new ArrayList<>(linesData.size());
+        for (String line : linesData) {
+            String[] coordPairStrArr = line.split("->");
+            Coordinate c1 = parseStrAsCoordinate(coordPairStrArr[0]);
+            Coordinate c2 = parseStrAsCoordinate(coordPairStrArr[1]);
+            lines.add(new Line(c1, c2));
+
+        }
+        return lines;
+    }
+
+    public void plotHorizontalVerticalLines(List<Line> lines, int[][] graph) {
+        //For each line, determine if it's horizontal or vertical
+        for (Line line : lines) {
+            if (line.isHorizontal()) {
+                //If it's horizontal, the y remains the same
+                for (int i=line.getXMin(); i<=line.getXMax(); i++) {
+                    graph[line.c1.y][i] += 1;
+                }
+            } else if (line.isVertical()) {
+                //If it's vertical, the x remains the same
+                for (int i=line.getYMin(); i<=line.getYMax(); i++) {
+                    graph[i][line.c1.x] += 1;
+                }
+            }
+        }
+    }
+
+    public void plotDiagonalLines(List<Line> lines, int[][] graph) {
+        //For each line, determine if it's horizontal or vertical
+        for (Line line : lines) {
+            if (line.isHorizontal() || line.isVertical()) {
+                continue;
+            }
+
+            int xRun = line.c2.x - line.c1.x;
+            int yRun = line.c2.y - line.c1.y;
+            int xSlope = xRun > 0 ? 1 : -1;
+            int ySlope = yRun > 0 ? 1 : -1;
+            //1. Take the first coordinate
+            //2. Increment the x coordinate
+            //3. Either increase/decrease the y coordinate (depending on the slope)
+            //Coordinate c = null;
+            for (int i=0; i<=Math.abs(xRun); i++) {
+                int x = (int) (line.c1.x + (i * xSlope));
+                int y = (int) (line.c1.y + (i * ySlope));
+                graph[x][y] += 1;
+
+				/*//TODO: Remove - debugging
+				c = new Coordinate(x, y);
+				if (i == 0) {
+					if (!line.c1.equals(c)) {
+						System.out.println("Expected starting coordinate of " + line.c1 + " but found " + c);
+						return;
+					}
+				} else if (i == absoluteRun) {
+					if (!line.c2.equals(c)) {
+						System.out.println("Expected ending coordinate of " + line.c2 + " but found " + c);
+						return;
+					}
+				}*/
+            }
+        }
+    }
+
+    int countOverlappingPoints(int[][] graph) {
+        int count = 0;
+        for (int i=0; i<graph.length; i++) {
+            for (int j=0; j<graph[i].length; j++) {
+                if (graph[i][j] > 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private Coordinate parseStrAsCoordinate(String pairStr) {
+        String[] xyPairArr = pairStr.split(",");
+        Coordinate coordinate = new Coordinate();
+        coordinate.x = Integer.valueOf(xyPairArr[0].trim());
+        coordinate.y = Integer.valueOf(xyPairArr[1].trim());
+        return coordinate;
+    }
+
+    long[] run(String fileName) throws FileNotFoundException, IOException {
+        //Parse the data as lines
+        List<Line> lines = parseData(fileName);
+        //int dimension = getMaxDimension(lines);
+        int dimension = 1000;
+        int[][] graph = new int[dimension][dimension];
+
+        //Plot the horizontal and vertical lines
+        plotHorizontalVerticalLines(lines, graph);
+        long partOne = countOverlappingPoints(graph);
+
+        //Plot the diagonal lines
+        plotDiagonalLines(lines, graph);
+        long partTwo = countOverlappingPoints(graph);
+
+        return new long[] {partOne, partTwo};
+    }
+
+    static class Line {
+        Coordinate c1;
+        Coordinate c2;
+
+        Line(Coordinate point1, Coordinate point2) {
+            this.c1 = point1;
+            this.c2 = point2;
+        }
+
+        int getXMin() {
+            return Math.min(c1.x, c2.x);
+        }
+
+        int getXMax() {
+            return Math.max(c1.x, c2.x);
+        }
+
+        int getYMin() {
+            return Math.min(c1.y, c2.y);
+        }
+
+        int getYMax() {
+            return Math.max(c1.y, c2.y);
+        }
+
+        boolean isHorizontal() {
+            return c2.y == c1.y;
+        }
+
+        boolean isVertical() {
+            return c2.x == c1.x;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s) -> (%s)", c1, c2);
+        }
+    }
+
+    static class Coordinate {
+        int x;
+        int y;
+
+        Coordinate() {
+            x = 0;
+            y = 0;
+        }
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", x, y);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null) {
+                return false;
+            }
+
+            Coordinate c = (Coordinate)o;
+            return this.x == c.x && this.y == c.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Day5 day = new Day5();
+        long[] solution = day.run("src/main/resources/day5_input.txt");
+        System.out.println(Arrays.toString(solution));
+    }
+}
