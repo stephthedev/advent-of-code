376e4a379d16eccc93e2c9212220664a0a225848
Stephanie Ortiz <stephthedev@gmail.com>
2024-10-24 16:17:23 -0400

    Day 1: Part 2

--- src/main/java/com/stephthedev/adventofcode/events2023/Day1.java
+++ src/main/java/com/stephthedev/adventofcode/events2023/Day1.java
@@ -3,9 +3,50 @@ package com.stephthedev.adventofcode.events2023;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Day1 {
+
+    private static Map<String, Integer> STR_NUM_MAP = Map.of(
+        "one", 1,
+        "two", 2,
+        "three", 3,
+        "four", 4,
+        "five", 5,
+        "six", 6,
+        "seven", 7,
+        "eight", 8,
+        "nine", 9
+    );
+
+    private List<Integer> findNumbersFromLine(String line) {
+        char[] chars = line.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        List<Integer> numbersFound = new ArrayList<>();
+        for (char alphanum : chars) {
+            if (Character.isDigit(alphanum)) {
+                numbersFound.add(Integer.parseInt(String.valueOf(alphanum)));
+                builder = new StringBuilder();
+            } else {
+                builder.append(alphanum);
+                String strFoundSoFar = builder.toString();
+                for (String numKey : STR_NUM_MAP.keySet()) {
+                    if (strFoundSoFar.contains(numKey)) {
+                        numbersFound.add(STR_NUM_MAP.get(numKey));
+
+                        //Hack: Keep the last character because it might be the first letter of the next word
+                        char lastChar = builder.charAt(builder.length() - 1);
+                        builder = new StringBuilder();
+                        builder.append(lastChar);
+                    }
+                }
+            }
+        }
+
+        return numbersFound;
+    }
 
     public int calculate(String inputFile) throws IOException {
         int totalSum = 0;
@@ -14,19 +55,14 @@ public class Day1 {
             String line = reader.readLine();
 
             while (line != null) {
-                List<Character> numbers = line.chars()
-                        .mapToObj(i -> (char) i)
-                        .filter(Character::isDigit)
-                        .toList();
+                List<Integer> numbersFound = findNumbersFromLine(line);
 
-                int firstNumberFound = numbers.stream()
+                int firstNumberFound = numbersFound.stream()
                         .findFirst()
-                        .map(num -> Integer.parseInt(String.valueOf(num)))
                         .orElseThrow(() -> new IllegalStateException("Expected to find a number on line"));
 
-                int lastNumberFound = numbers.stream()
+                int lastNumberFound = numbersFound.stream()
                         .reduce((first, second) -> second)
-                        .map(num -> Integer.parseInt(String.valueOf(num)))
                         .orElseGet(() -> firstNumberFound);
 
                 int twoDigitNumber = Integer.parseInt(String.format("%d%d", firstNumberFound, lastNumberFound));
