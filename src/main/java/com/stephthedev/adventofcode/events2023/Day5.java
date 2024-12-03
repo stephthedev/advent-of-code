bcc12476e220d899e9b1eb1cc91f8de7785d5e32
Stephanie Ortiz <stephthedev@gmail.com>
2024-12-02 23:10:59 -0500

    Clean up 2023/day5

--- src/main/java/com/stephthedev/adventofcode/events2023/Day5.java
+++ src/main/java/com/stephthedev/adventofcode/events2023/Day5.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
@@ -14,7 +13,6 @@ public class Day5 {
 
     private static final Pattern RANGE_PATTERN = Pattern.compile("(([0-9]+) ([0-9]+) ([0-9]+))");
     private static final Pattern MAP_PATTERN = Pattern.compile("([a-z]+-to-[a-z]+) map:.*");
-    private static final Pattern SEEDS_PATTERN = Pattern.compile("seeds: [0-9]");
 
     public Almanac precompute(String inputFile) throws IOException {
         List<Long> seeds = new ArrayList<>();
@@ -45,11 +43,15 @@ public class Day5 {
                 } else if (!line.trim().isEmpty()) {
                     List<AlmanacLine> lines = RANGE_PATTERN.matcher(line)
                             .results()
-                            .map(matchResult -> new AlmanacLine(
-                                    Long.parseLong(matchResult.group(2)),
-                                    Long.parseLong(matchResult.group(3)),
-                                    Long.parseLong(matchResult.group(4))
-                            ))
+                            .map(matchResult -> {
+                                long destination = Long.parseLong(matchResult.group(2));
+                                long source = Long.parseLong(matchResult.group(3));
+                                long rangeLength = Long.parseLong(matchResult.group(4));
+                                return new AlmanacLine(
+                                        new Range(destination, destination + rangeLength),
+                                        new Range(source, source + rangeLength)
+                                );
+                            })
                             .toList();
 
                     switch (almanacMapType) {
@@ -78,90 +80,108 @@ public class Day5 {
         );
     }
 
-    public long findLowestLocation(Collection<Long> seeds, Almanac almanac) {
-        Map<Long, Long> seedToLocationMap = seeds.stream()
-                .collect(Collectors.toMap(
-                        seed -> seed,
-                        seed -> almanac.seedToSoilRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seed))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seed)  //seed-soil
-                ))
-                /*.entrySet()
+
+    public long findLowestLocation(List<Range> seeds, Almanac almanac) {
+        Map<Range, List<Range>> locationBySeedMap =  seeds.stream()
+            .collect(Collectors.toMap(
+                    seed -> seed,
+                    seed -> getPossibleDestinations(seed, almanac.seedToSoilRange) //seed-soil
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedSoilEntry ->  seedSoilEntry.getValue().stream()
+                            .map(soilRange -> getPossibleDestinations(soilRange, almanac.soilToFertilizerRange))
+                            .flatMap(Collection::stream)
+                            .toList() //seed-fertilizer
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedFertilizerEntry -> seedFertilizerEntry.getValue().stream()
+                            .map(fertilizerRange -> getPossibleDestinations(fertilizerRange, almanac.fertilizerToWaterRange))
+                            .flatMap(Collection::stream)
+                            .toList()  //seed-water
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedWaterEntry -> seedWaterEntry.getValue().stream()
+                            .map(waterRange -> getPossibleDestinations(waterRange, almanac.waterToLightRange))
+                            .flatMap(Collection::stream)
+                            .toList()  //seed-light
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedLightEntry -> seedLightEntry.getValue().stream()
+                            .map(lightRange -> getPossibleDestinations(lightRange, almanac.lightToTemperatureRange))
+                            .flatMap(Collection::stream)
+                            .toList()  //seed-temperature
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedTempEntry -> seedTempEntry.getValue().stream()
+                            .map(tempRange -> getPossibleDestinations(tempRange, almanac.temperatureToHumidityRange))
+                            .flatMap(Collection::stream)
+                            .toList()  //seed-humidity
+            ))
+            .entrySet()
+            .stream()
+            .collect(Collectors.toMap(
+                    Map.Entry::getKey,
+                    seedHumidityEntry -> seedHumidityEntry.getValue().stream()
+                            .map(humidityRange -> getPossibleDestinations(humidityRange, almanac.humidityToLocationRange))
+                            .flatMap(Collection::stream)
+                            .toList()  //seed-location
+            ))
+        ;
+
+        return locationBySeedMap
+                .values()
                 .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedSoilEntry -> almanac.soilToFertilizerRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedSoilEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedSoilEntry.getValue())  //seed-fertilizer
-                ))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedFertilizerEntry -> almanac.fertilizerToWaterRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedFertilizerEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedFertilizerEntry.getValue())  //seed-water
-                ))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedLightEntry -> almanac.waterToLightRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedLightEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedLightEntry.getValue())  //seed-light
-                ))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedLightEntry -> almanac.lightToTemperatureRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedLightEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedLightEntry.getValue())  //seed-temp
-                ))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedTempEntry -> almanac.temperatureToHumidityRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedTempEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedTempEntry.getValue())  //seed-humidity
-                ))
-                .entrySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        seedHumidityEntry -> almanac.humidityToLocationRange.stream()
-                                .map(almanacLine -> almanacLine.getDestinationForSource(seedHumidityEntry.getValue()))
-                                .filter(Optional::isPresent)
-                                .map(Optional::get)
-                                .findFirst()
-                                .orElse(seedHumidityEntry.getValue())  //seed-humidity
-                ))*/;
-        System.out.println("SEED-LOCATION: " + seedToLocationMap);
-        return seedToLocationMap.values()
-                .stream()
-                .mapToLong(v -> v)
+                .flatMap(Collection::stream)
+                .mapToLong(Range::start)
                 .min()
                 .orElse(Long.MIN_VALUE);
 
+    }
+
+    List<Range> getPossibleDestinations(Range sourceRange, Collection<AlmanacLine> sourceToDestinationRange) {
+        @SuppressWarnings("unchecked")
+        List<Range> possibleDestinations = (List<Range>) sourceToDestinationRange.stream()
+                .map(almanacLine -> {
+                    if(almanacLine.sourceRange.containsSubRange(sourceRange)) {
+                        long startDelta = Math.abs(sourceRange.start - almanacLine.sourceRange.start);
+                        long endDelta = Math.abs(sourceRange.end - almanacLine.sourceRange.start);
+                        long possibleDestinationStart = almanacLine.destinationRange.start + startDelta;
+                        long possibleDestinationEnd = almanacLine.destinationRange.start + endDelta;
+                        if (almanacLine.destinationRange.containsSubRange(possibleDestinationStart, possibleDestinationStart)) {
+                            return Optional.of(new Range(possibleDestinationStart, possibleDestinationStart));
+                        }
+                    }
+                    return Optional.empty();
+                })
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .toList();
+        return possibleDestinations.isEmpty() ? Collections.singletonList(sourceRange) : possibleDestinations;
+    }
+
+    record Range(long start, long end) {
+        boolean containsSubRange(long start, long end) {
+            return start >= this.start && end <= this.end;
+        }
+
+        boolean containsSubRange(Range range) {
+            return range.start >= this.start && range.end <= this.end;
+        }
     }
 
     record Almanac(List<Long> seeds,
@@ -174,22 +194,8 @@ public class Day5 {
                    Collection<AlmanacLine> humidityToLocationRange) {
     }
 
-    record AlmanacLine(long destinationRangeStart, long sourceRangeStart, long rangeLength ) {
+    record AlmanacLine(Range destinationRange, Range sourceRange) {
 
-        Optional<Long> getDestinationForSource(long source) {
-            long sourceRangeEnd = sourceRangeStart + rangeLength;
-            if (source >= sourceRangeStart && source <= sourceRangeEnd) {
-                long delta = source - sourceRangeStart;
-                long possibleDestination = destinationRangeStart + delta;
-                long destinationRangeEnd = destinationRangeStart + rangeLength;
-                if (possibleDestination >= destinationRangeStart && possibleDestination <= destinationRangeEnd) {
-                    //System.out.println("Found destination: " + possibleDestination);
-                    return Optional.of(possibleDestination);
-                }
-            }
-
-            return Optional.empty();
-        }
     }
 
     enum AlmanacMapType {
@@ -218,36 +224,26 @@ public class Day5 {
 
     public static void main(String[] args) throws IOException {
         Day5 day5 = new Day5();
-        Almanac almanac = day5.precompute("aoc2023/day5_input.txt");
+        Almanac almanac = day5.precompute("scratch.txt");
 
-        long part1Lowest = day5.findLowestLocation(almanac.seeds, almanac);
         //Scratch: 35, Day5: 282277027
+        List<Range> exactSeedRanges = almanac.seeds.stream().map(seed -> new Range(seed, seed)).toList();
+        long part1Lowest = day5.findLowestLocation(exactSeedRanges, almanac);
         System.out.println("Part 1 Lowest Location: " + part1Lowest);
 
-        long previousSeedStart = 0;
-        long maxBatchSize = 1000;
-        List<Long> lowestLocations = new ArrayList<>();
+        //Scratch: 35, Day5:
+        List<Range> seedRanges = new ArrayList<>();
         for (int i=0; i<almanac.seeds.size(); i++) {
-            if (i % 2 == 0) {
-                previousSeedStart = almanac.seeds.get(i);
-            } else {
-                long range = almanac.seeds.get(i);
-                int totalBatches = (int) (range / maxBatchSize);
-                
-                
-                kdk
-                
-                long lowestLocation = IntStream.rangeClosed(1, totalBatches)
-                        .mapToLong(batch -> {
-                            long upperRange = previousSeedStart + (batch * maxBatchSize);
-                        })
-                        .min();
-//                long lowestLocation = day5.findLowestLocation(Collections.emptyList(), almanac);
-//                lowestLocations.add(lowestLocation);
-
+            if ((i % 2) == 1) {
+                long seedStart = almanac.seeds.get(i - 1);
+                long rangeLength = almanac.seeds.get(i);
+                long seedEnd = (seedStart - 1) + rangeLength;
+                seedRanges.add(new Range(seedStart, seedEnd));
             }
         }
-        long part2Lowest = lowestLocations.stream().mapToLong(v -> v).min().orElse(Long.MIN_VALUE);
+
+        //Scratch: 46
+        long part2Lowest = day5.findLowestLocation(seedRanges, almanac);
         System.out.println("Part 2 Lowest Location: " + part2Lowest);
     }
 }
