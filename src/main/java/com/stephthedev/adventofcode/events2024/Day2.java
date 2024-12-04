c156ece1552c7ab999f9244053c9fe2ae27e5e51
Stephanie Ortiz <stephthedev@gmail.com>
2024-12-04 08:48:16 -0500

    Incomplete 2024 day 2 part 2

--- src/main/java/com/stephthedev/adventofcode/events2024/Day2.java
+++ src/main/java/com/stephthedev/adventofcode/events2024/Day2.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class Day2 {
 
@@ -27,30 +28,96 @@ public class Day2 {
 
     public long countSafeReports(List<List<Integer>> reports) {
         return reports.stream()
-                .filter(report -> {
-                    Boolean isIncreasing = null;
-                    for (int i=0; i<report.size(); i++) {
-                        int currNumber = report.get(i);
-                        if (i + 1 < report.size()) {
-                            int nextNumber = report.get(i + 1);
-                            if (isIncreasing == null) {
-                                isIncreasing = (nextNumber - currNumber) > 0;
-                            }
-                            int diff = Math.abs(nextNumber - currNumber);
-                            if (!(diff >= 1 && diff <= 3)) {
-                                return false;
-                            } else if (isIncreasing && !(nextNumber > currNumber)) {
-                                return false;
-                            } else if (!isIncreasing && !(nextNumber < currNumber)) {
-                                return false;
-                            }
-                        }
-                    }
-
-                    System.out.printf("Report %s is safe \n", report);
-                    return true;
-                })
+                .filter(this::isSafe)
                 .count();
+    }
+
+    public long countFixableSafeReports(List<List<Integer>> reports) {
+        return reports.stream()
+                .map(this::fixReport)
+                .filter(this::isSafe)
+                .count();
+    }
+
+    boolean isSafe(List<Integer> report) {
+        for (int i=1; i<report.size(); i++) {
+            if ((i+1) < report.size()) {
+                int left = report.get(i - 1);
+                int middle = report.get(i);
+                int right = report.get(i + 1);
+
+                if ((left < middle && isWithinTolerance(middle - left))
+                        && (middle < right && isWithinTolerance(right - middle))) {
+                    //TODO:
+                } else if ((left > middle && isWithinTolerance(left - middle))
+                        && (middle > right && isWithinTolerance(middle - right))) {
+                    //TODO
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    List<Integer> fixReport(List<Integer> report) {
+        int ascendingCount = 0, descendingCount = 0;
+
+        for (int i=1; i<report.size(); i++) {
+            int prev = report.get(i - 1);
+            int curr = report.get(i);
+            if (prev < curr) {
+                ascendingCount++;
+            } else if (prev > curr) {
+                descendingCount++;
+            }
+        }
+
+        boolean isAscending = ascendingCount > descendingCount;
+
+        Stack<Integer> fixedReport = new Stack<>();
+        boolean hasOmissions = false;
+        for (int i=1; i<report.size(); i++) {
+            int prev = report.get(i - 1);
+            int curr = report.get(i);
+            int prevCurrDiff = isAscending ? curr - prev : prev - curr;
+
+            if (prevCurrDiff == 0) {
+                //Omit duplicates
+                hasOmissions = true;
+            } else if (isWithinTolerance(prevCurrDiff)) {
+                fixedReport.add(prev);
+                hasOmissions = true;
+            } else if (isAscending && prevCurrDiff < 0) {
+                if (!fixedReport.isEmpty() && fixedReport.peek() < prev) {
+                    fixedReport.add(prev);
+                    hasOmissions = true;
+                }
+            } else if (!isAscending && prevCurrDiff > 0) {
+                if (!fixedReport.isEmpty() && fixedReport.peek() > prev) {
+                    fixedReport.add(prev);
+                    hasOmissions = true;
+                }
+            }
+
+            if (i == report.size() - 1) {
+                int lastSaved = fixedReport.peek();
+                int lastCurrDiff = isAscending ? curr - lastSaved : lastSaved - curr;
+                int lastPrevDiff = isAscending ? prev - lastSaved : lastSaved - prev;
+                if (isWithinTolerance(lastCurrDiff)) {
+                    fixedReport.add(curr);
+                } else if (fixedReport.peek() != prev && isWithinTolerance(lastPrevDiff)) {
+                    fixedReport.add(prev);
+                }
+            }
+        }
+
+        return fixedReport;
+    }
+
+
+    public boolean isWithinTolerance(int diff) {
+        return diff >= 1 && diff <= 3;
     }
 
     public static void main(String[] args) throws IOException {
@@ -58,5 +125,11 @@ public class Day2 {
         List<List<Integer>> reports = day2.precompute("2024/day2_input.txt");
         long totalSafeReports = day2.countSafeReports(reports);
         System.out.println("Total safe reports: " + totalSafeReports);
+
+        List<Integer> sample = List.of(49, 47, 44, 46, 41);
+        System.out.println("Before: " + sample);
+        System.out.println("After: " + day2.fixReport(sample));
+        long totalSafeFixedReports = day2.countFixableSafeReports(reports);
+        System.out.println("Total fixed safe reports: " + totalSafeFixedReports);
     }
 }
