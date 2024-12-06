9e06ff65553d1161b8d1458cef76faedfe7f8a75
Stephanie Ortiz <stephthedev@gmail.com>
2024-12-06 09:39:58 -0500

    Day 4 part 2

--- src/main/java/com/stephthedev/adventofcode/events2024/Day4.java
+++ src/main/java/com/stephthedev/adventofcode/events2024/Day4.java
@@ -3,34 +3,80 @@ package com.stephthedev.adventofcode.events2024;
 import com.stephthedev.adventofcode.utils.FileParserUtil;
 
 import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Day4 {
 
-    public int part1(char[][] board, String word) {
-        int count = 0;
-        for (Direction direction : Direction.values()) {
-            count += searchAndCount(board, word, direction);
-        }
+    private static final String XMAS = "XMAS";
+    private static final String MAS = "MAS";
 
-        return count;
+    public int part1(char[][] board) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        for (Direction direction : Direction.values()) {
+            coordinates.addAll(searchAndCount(board, XMAS, direction));
+        }
+        return coordinates.size();
     }
 
-    private int searchAndCount(char[][] board, String word, Direction direction) {
+    public long part2(char[][] board) {
+        Set<Direction> validDirections = Set.of(Direction.NORTH_EAST, Direction.SOUTH_EAST, Direction.SOUTH_WEST, Direction.NORTH_WEST);
+
+        List<Coordinate> middleLetterCoordinates = new ArrayList<>();
+
+        for (Direction direction : validDirections) {
+            searchAndCount(board, MAS, direction)
+                    .stream()
+                    .map(coordinate -> switch(direction) {
+                        case NORTH, SOUTH, EAST, WEST -> null;
+
+                        case NORTH_EAST -> new Coordinate(
+                                coordinate.row - 1,
+                                coordinate.col + 1
+                        );
+                        case SOUTH_EAST -> new Coordinate(
+                                coordinate.row + 1,
+                                coordinate.col + 1
+                        );
+                        case SOUTH_WEST -> new Coordinate(
+                                coordinate.row + 1,
+                                coordinate.col - 1
+                        );
+                        case NORTH_WEST -> new Coordinate(
+                                coordinate.row - 1,
+                                coordinate.col - 1
+                        );
+                    })
+                    .forEach(middleLetterCoordinates::add);
+        }
+
+        long total = middleLetterCoordinates.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .values()
+                .stream()
+                .filter(count -> count > 1)
+                .reduce(0L, Long::sum);
+
+        //TODO: I think this is doubled up counting all the duplicate As, we need to do divide to get the actual number of crosses
+        return total / 2L;
+    }
+
+    private List<Coordinate> searchAndCount(char[][] board, String word, Direction direction) {
         boolean[][] visited = new boolean[board.length][board[0].length];
 
-        int count = 0;
+        List<Coordinate> startingCoordinates = new ArrayList<>();
         for (int row = 0; row<board.length; row++) {
             for (int col = 0; col<board[row].length; col++) {
                 if (board[row][col] == word.charAt(0)) {
                     if (isNextLetterFound(board, row, col, word, 0, visited, direction)) {
-                        System.out.printf("Found word at [%d, %d] going %s \n", row, col, direction);
-                        count++;
+                        startingCoordinates.add(new Coordinate(row, col));
                     }
                 }
             }
         }
 
-        return count;
+        return startingCoordinates;
     }
 
     private boolean isNextLetterFound(char[][] board,
@@ -95,10 +141,16 @@ public class Day4 {
         NORTH_WEST
     }
 
+    record Coordinate(int row, int col) {
+    }
+
     public static void main(String[] args) throws IOException {
         Day4 day4 = new Day4();
         char[][] crossword = FileParserUtil.parseFile("2024/day4_input.txt");
-        int xmasOccurenceCount = day4.part1(crossword, "XMAS");
+        int xmasOccurenceCount = day4.part1(crossword);
         System.out.println("Part 1: " + xmasOccurenceCount);
+
+        long part2 = day4.part2(crossword);
+        System.out.println("Part 2: " + part2);
     }
 }
